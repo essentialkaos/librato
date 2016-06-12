a7c1de0e23f15f8f42749d1e8e121cb0041e7bef
Anton Novojilov <andy@essentialkaos.com>
2016-06-12 18:37:21 -0400

    Version 2

---
+++ examples/collector_example.go
@@ -0,0 +1,52 @@
+package main
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+import (
+	"fmt"
+	"time"
+
+	"pkg.re/essentialkaos/ek.v1/rand"
+
+	"github.com/essentialkaos/librato"
+)
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+func main() {
+	librato.Mail = "mail@domain.com"
+	librato.Token = "abcd1234abcd1234abcd1234abcd1234abcd1234abcd1234abcd1234abcd1234"
+
+	collector := librato.NewCollector(time.Minute, collectSomeMetrics)
+	collector.ErrorHandler = errorHandler
+
+	for {
+		time.Sleep(time.Hour)
+	}
+}
+
+func collectSomeMetrics() []librato.Measurement {
+	return []librato.Measurement{
+		librato.Gauge{
+			Name:  "example:gauge_1",
+			Value: rand.Int(1000),
+		},
+		librato.Gauge{
+			Name:   "example:gauge_2",
+			Value:  float64(rand.Int(1000)) / float64(rand.Int(20)),
+			Source: "go_librato_example",
+		},
+		librato.Counter{
+			Name:  "example:counter_1",
+			Value: rand.Int(1000),
+		},
+	}
+}
+
+func errorHandler(errs []error) {
+	fmt.Println("Errors:")
+
+	for _, err := range errs {
+		fmt.Printf("  %v\n", err)
+	}
+}
