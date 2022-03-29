8261fc412b801d3ed39b68d3f15df2f39dbd996b
Anton Novojilov <andy@essentialkaos.com>
2022-03-30 00:59:35 +0300

    Migrate to go modules + Improve GitHub actions workflows

--- examples/annotations_example.go
+++ examples/annotations_example.go
@@ -6,7 +6,7 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/essentialkaos/librato"
+	"github.com/essentialkaos/librato/v10"
 )
 
 // ////////////////////////////////////////////////////////////////////////////////// //
